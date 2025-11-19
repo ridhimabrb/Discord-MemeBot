0ea11e5221cf4e07799f9c58d8f0c14d034ba9bc
Ridhima <pantridhima2323@gmail.com>
2025-11-20 05:17:57 +0530

    Initial bot setup

---
+++ bot.py
@@ -0,0 +1,31 @@
+import discord
+import requests
+import json
+from dotenv import load_dotenv
+import os
+
+def get_meme():
+  response = requests.get('https://meme-api.com/gimme')
+  json_data = json.loads(response.text)
+  return json_data['url']
+
+class MyClient(discord.Client):
+  async def on_ready(self):
+    print('Logged on as {0}!'.format(self.user))
+
+  async def on_message(self, message):
+     if message.author == self.user:
+        return
+
+     if message.content.startswith('$meme'):
+        await message.channel.send(get_meme())
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+client.run(TOKEN)
